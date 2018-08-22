59a85ee248e06c4e652f4893ecfd9ddd9e6030a3
sonlv <vanson1904@gmail.com>
2018-08-23 00:02:46 +0700

    build java -jar by command line

---
+++ Run_Java_By_jar.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+BASE_DIR=/home/mrson/NewpostJava
+BATCH_HOME=$BASE_DIR/NewpostBatch
+MAIN_CLASS_JAVA=$BATCH_HOME/printPDF/VibloPdf.java
+TAGET_CLASS=$BATCH_HOME/target/classes
+
+#FUNCTION
+function setClassPathDefault(){
+    CLASSPATH="$BATCH_HOME:$TAGET_CLASS";
+}
+
+function add()
+{
+    LIB_PATH="$LIB_PATH:$1"
+}
+
+function setClassPath(){
+    CLASSPATH="$CLASSPATH:$1";
+}
+#FUNCTION
+
+#setDeafault
+setClassPathDefault;
+
+#set lib
+for i in $BATCH_HOME/lib/*.jar
+do
+    add $i;
+done
+
+for i in $BATCH_HOME/lib/*.zip
+do
+    add $i;
+done
+
+
+# set lib path into classpath
+setClassPath $LIB_PATH;
+
+export classpath=$CLASSPATH;
+
+javac -cp $classpath $MAIN_CLASS_JAVA -d $TAGET_CLASS
+#java  -cp $classpath printPDF.VibloPdf
+jar -cfvm VibloPdf.jar manifest.txt $TAGET_CLASS/printPDF/VibloPdf
+
+java -jar VibloPdf.jar
+
+
+
+
